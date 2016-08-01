66c8b79afe0d790d1b9d2b5a26eccc5a5c5706ab
christian <christian@nethost.homelan>
2016-08-01 04:06:37 +0200

    revision main script

--- sysdev.sh
+++ sysdev.sh
@@ -78,7 +78,7 @@ if [ $1 == "install" ]; then
 	cp sysdev/bashrc  ~/.bashrc
 	cp sysdev/profile ~/.profile
 	cp sysdev/bash_logout ~/.bash_logout
-	cp sysdev/bash_conf/* ~/.bash_conf
+	cp --recursively sysdev/bash_conf/ ~/.bash_conf
 	cp sysdev/config/ranger/rc.conf ~/.config/rc.conf
 	cp sysdev/config/htop/htoprc ~/.config/htop/htoprc
 
