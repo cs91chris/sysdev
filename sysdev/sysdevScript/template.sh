9f6ec763c54cdf37401bc82d8c75d577fc24b1cf
christian <christian@debianLaptop.homelan>
2016-08-08 17:29:11 +0200

    add pyrc and models

--- sysdev/sysdevScript/template.sh
+++ sysdev/sysdevScript/template.sh
@@ -5,12 +5,12 @@ source ~/sysdevScript/color.conf 2> /dev/null
 
 if [ -f ~/sysdevModels/$1.$1 ]; then
 	cp ~/sysdevModels/$1.$1 ./$2.$1
-	echo -e "$2 ${green}created${reset}"
+	vim $2.$1
 
 else
 	if [ -f ~/sysdevModels/$1 ]; then
 		cp ~/sysdevModels/$1 ./$2
-		echo -e "$2 ${green}created${reset}"
+		vim $2
 
 	else
 		echo -e "template does ${red}not exist${reset}!"
