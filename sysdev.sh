092a1225e7b3c8d5ba4d41d30974411befc2f308
cs91chris <cs91chris@voidbrain.me>
2020-05-20 22:14:28 +0200

    using functions for clean code

--- sysdev.sh
+++ sysdev.sh
@@ -11,15 +11,105 @@ VIM_BUNDLE=${HOME}/.vim/bundle
 
 VER_GIT_PROMPT="2.7.1"
 VER_VUNDLE="v0.10.2"
-VER_TMUX_CONF="d6f0f647dd68561ed010f83d8d226383aebfb805"
 VER_LAZY_GIT="0.20.4"
+VER_TMUX_CONF="d6f0f647dd68561ed010f83d8d226383aebfb805"  # commit
 
 REPO_VUNDLE="https://github.com/VundleVim/Vundle.vim.git"
 REPO_TMUX_CONF="https://github.com/gpakosz/.tmux.git"
 REPO_GIT_PROMPT="https://github.com/magicmonty/bash-git-prompt.git"
 REPO_LAZY_GIT="https://github.com/jesseduffield/lazygit/releases/download"
-REPO_LAZY_GIT_NIX="${REPO_LAZY_GIT}/v${VER_LAZY_GIT}/lazygit_${VER_LAZY_GIT}_Linux_x86_64.tar.gz"
-REPO_LAZY_GIT_WIN="${REPO_LAZY_GIT}/v${VER_LAZY_GIT}/lazygit_${VER_LAZY_GIT}_Windows_x86_64.zip"
+
+
+function install_base {
+	mkdir -p ${HOME}/{.log,.bin}
+	cd ${SYSDEV_DIR}/sysdev > /dev/null
+
+	for x in $(ls); do
+		[ -d ${HOME}/.${x} ] && tmp="${x}/*" || tmp="${x}" 
+		cp --verbose --recursive ${tmp} ${HOME}/.${x} &>> ${FILE_LOG} || ERR_EXIT=1
+	done
+	cd - > /dev/null
+
+	if [[ -x $(which ranger 2> /dev/null) ]]; then
+		ranger --copy-config=scope &>> ${FILE_LOG} || ERR_EXIT=1
+	fi
+}
+
+
+function install_cheat {
+	curl -s "https://cheat.sh/:bash_completion" > ${HOME}/.bash_conf/cht_completion 2>> ${FILE_LOG} || ERR_EXIT=1
+	curl -s "https://cht.sh/:cht.sh" > ${HOME}/.script/cht 2>> ${FILE_LOG} || ERR_EXIT=1
+}
+
+
+function install_tmux_conf {
+	if [[ -x $(which tmux 2> /dev/null ) ]]; then
+		echo -e "${orange}Installing tmux${reset} configuration..."
+
+		rm -vrf ${HOME}/.tmux &>> ${FILE_LOG} || ERR_EXIT=1
+		git clone ${REPO_TMUX_CONF} ${HOME}/.tmux --depth 1 >> ${FILE_LOG} 2>&1 && {
+			cd ${HOME}/.tmux > /dev/null
+			git checkout -b tmp ${VER_TMUX_CONF}
+			cp .tmux.conf ${HOME}
+			cp .tmux.conf.local ${HOME}
+			cd - > /dev/null
+		} || ERR_EXIT=1
+	fi
+}
+
+
+function install_git_prompt {
+	rm -vrf ${HOME}/.bash-git-prompt &>> ${FILE_LOG}
+	git clone ${REPO_GIT_PROMPT} --branch ${VER_GIT_PROMPT} ${HOME}/.bash-git-prompt --depth 1 &>> ${FILE_LOG}
+}
+
+
+function install_lazy_git {
+	case "$(cat /proc/version)" in
+		MINGW*)
+			REPO_LAZY_GIT="${REPO_LAZY_GIT}/v${VER_LAZY_GIT}/lazygit_${VER_LAZY_GIT}_Windows_x86_64.zip"
+			DECOMPRESSOR="unzip -o"
+			EXT="zip"
+		;;
+		Linux*)
+			REPO_LAZY_GIT="${REPO_LAZY_GIT}/v${VER_LAZY_GIT}/lazygit_${VER_LAZY_GIT}_Linux_x86_64.tar.gz"
+			DECOMPRESSOR="tar -xzf"
+			EXT="tar.gz"
+		;;
+		*)
+			REPO_LAZY_GIT=""
+			echo -e "not supported OS: $(cat /proc/version)"
+		;;
+	esac
+
+	if [[ ! -z "${REPO_LAZY_GIT}" ]]; then
+		rm -vrf ${HOME}/.bin/lazygit* &>> ${FILE_LOG}
+		wget -O ${HOME}/.bin/lazygit.${EXT} "${REPO_LAZY_GIT}" 2>> ${FILE_LOG} > /dev/null && {
+			cd ${HOME}/.bin > /dev/null
+			${DECOMPRESSOR} lazygit.${EXT} &>> ${FILE_LOG}
+			rm -vrf LICENSE README.md &>> ${FILE_LOG}
+			cd - > /dev/null
+		} || ERR_EXIT=1
+	fi
+}
+
+
+function install_vim_plugins {
+	if [[ -x $(which vim 2> /dev/null) ]]; then
+		echo -e "${orange}Setting vim${reset} configurations..."
+
+		rm -vrf ${VIM_BUNDLE}/Vundle.vim &>> ${FILE_LOG} || ERR_EXIT=1
+		git clone ${REPO_VUNDLE} ${VIM_BUNDLE}/Vundle.vim --branch ${VER_VUNDLE} --depth 1 &>> ${FILE_LOG} && {
+			vim +PluginInstall +qall
+
+			echo -e "${orange}compiling${reset} youCompleteMe..."
+
+			cd ${VIM_BUNDLE}/YouCompleteMe > /dev/null
+			[[ -x $(which python3 2> /dev/null) ]] && PYTHON_CMD=python3 || PYTHON_CMD=python
+			${PYTHON_CMD} install.py --clang-completer 2>> ${FILE_LOG} > /dev/null || ERR_EXIT=1
+		} || ERR_EXIT=1
+	fi
+}
 
 
 if [[ $1 == "--help" ]]; then
@@ -36,85 +126,15 @@ case "${ANS}" in
 	*) exit 1 ;;
 esac
 
-
 echo -e "${orange}Installing${reset} configurations and scripts..."
-
-mkdir -p ${HOME}/{.log,.bin}
-cd ${SYSDEV_DIR}/sysdev > /dev/null
-
-for x in $(ls); do
-	[ -d ${HOME}/.${x} ] && tmp="${x}/*" || tmp="${x}" 
-	cp --verbose --recursive ${tmp} ${HOME}/.${x} &>> ${FILE_LOG} || ERR_EXIT=1
-done
-cd - > /dev/null
-
-if [[ -x $(which ranger 2> /dev/null) ]]; then
-    ranger --copy-config=scope &>> ${FILE_LOG} || ERR_EXIT=1
-fi
-
-curl -s "https://cheat.sh/:bash_completion" > ${HOME}/.bash_conf/cht_completion 2>> ${FILE_LOG} || ERR_EXIT=1
-curl -s "https://cht.sh/:cht.sh" > ${HOME}/.script/cht 2>> ${FILE_LOG} || ERR_EXIT=1
+install_base
+install_cheat
+install_tmux_conf
+install_git_prompt
+install_lazy_git
+install_vim_plugins
 
 chmod +x ${HOME}/{.script,.bin}/* >> ${FILE_LOG} 2>&1
-
-if [[ -x $(which tmux 2> /dev/null ) ]]; then
-    echo -e "${orange}Installing tmux${reset} configuration..."
-
-	rm -vrf ${HOME}/.tmux &>> ${FILE_LOG} || ERR_EXIT=1
-    git clone ${REPO_TMUX_CONF} ${HOME}/.tmux --depth 1 >> ${FILE_LOG} 2>&1 && {
-		cd ${HOME}/.tmux > /dev/null
-		git checkout -b tmp ${VER_TMUX_CONF}
-        cp .tmux.conf ${HOME}
-        cp .tmux.conf.local ${HOME}
-		cd - > /dev/null
-    } || ERR_EXIT=1
-fi
-
-rm -vrf ${HOME}/.bash-git-prompt &>> ${FILE_LOG}
-git clone ${REPO_GIT_PROMPT} --branch ${VER_GIT_PROMPT} ${HOME}/.bash-git-prompt --depth 1 &>> ${FILE_LOG}
-
-case "$(cat /proc/version)" in
-	MINGW*)
-		REPO_LAZY_GIT=${REPO_LAZY_GIT_WIN}
-		DECOMPRESSOR="unzip -o"
-		EXT="zip"
-	;;
-	Linux*)
-		REPO_LAZY_GIT=${REPO_LAZY_GIT_NIX}
-		DECOMPRESSOR="tar -xzf"
-		EXT="tar.gz"
-	;;
-	*)
-		REPO_LAZY_GIT=""
-		echo -e "not supported OS: $(cat /proc/version)"
-	;;
-esac
-
-if [[ ! -z "${REPO_LAZY_GIT}" ]]; then
-	rm -vrf ${HOME}/.bin/lazygit* &>> ${FILE_LOG}
-	wget -O ${HOME}/.bin/lazygit.${EXT} "${REPO_LAZY_GIT}" 2>> ${FILE_LOG} > /dev/null && {
-		cd ${HOME}/.bin > /dev/null
-		${DECOMPRESSOR} lazygit.${EXT} &>> ${FILE_LOG}
-		rm -vrf LICENSE README.md &>> ${FILE_LOG}
-		cd - > /dev/null
-	} || ERR_EXIT=1
-fi
-
-if [[ -x $(which vim 2> /dev/null) ]]; then
-    echo -e "${orange}Setting vim${reset} configurations..."
-
-    rm -vrf ${VIM_BUNDLE}/Vundle.vim &>> ${FILE_LOG} || ERR_EXIT=1
-    git clone ${REPO_VUNDLE} ${VIM_BUNDLE}/Vundle.vim --branch ${VER_VUNDLE} --depth 1 &>> ${FILE_LOG} && {
-		vim +PluginInstall +qall
-
-		echo -e "${orange}compiling${reset} youCompleteMe..."
-
-		cd ${VIM_BUNDLE}/YouCompleteMe > /dev/null
-		[[ -x $(which python3 2> /dev/null) ]] && PYTHON_CMD=python3 || PYTHON_CMD=python
-		${PYTHON_CMD} install.py --clang-completer 2>> ${FILE_LOG} > /dev/null || ERR_EXIT=1
-	} || ERR_EXIT=1
-fi
-
 
 if [[ ${ERR_EXIT} -eq 1 ]]; then
 	echo -e "${red}"
